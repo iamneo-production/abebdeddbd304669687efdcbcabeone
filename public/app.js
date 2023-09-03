c547596620a47245f84273f95b9ea3c6a829377e
Iamneo <techadmin@iamneo.ai>
2023-09-03 05:42:48 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -19,6 +19,31 @@ function handleMove(button) {
 
 function checkForWinOrDraw() {
     const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    for (const combination of winningCombinations) {
+        const[a,b,c] = combination;
+        if (
+            buttons[a].textContent&&
+            buttons[a].textContent===buttons[b].textContent &&
+            buttons[a].textContent===buttons[c].textContent
+        ) {
+            gameOver = true;
+            resultText.textContent = `Player ${buttons[a].textContent} wins!`;
+            resetButton.disabled = false;
+            return;
+        }
+    }
+
+    if (moves===buttons.length) {
         
-    ]
+    }
 }
